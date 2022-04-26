f99b38f0d5159393eea334bdeec22fff1c8c4ee3
Jess Phillips <tripphillips5@gmail.com>
2022-04-26 08:57:47 -0500

    test(StackTests): add test cases to cover `Stack` methods

--- StackTests/StackTests.swift
+++ StackTests/StackTests.swift
@@ -10,11 +10,134 @@ import XCTest
 
 class StackTests: XCTestCase {
 
+    func test_initialize_withParam() {
+        let sut: Stack<Int> = Stack<Int>([1,2,3,4,5])
+        let expected =
+        """
+        ----top----
+        5
+        4
+        3
+        2
+        1
+        -----------
+        """
+        XCTAssertEqual(expected, sut.debugDescription)
+    }
+    
+    func test_initialize_withLiteral() {
+        let sut: Stack<Int> = [1,2,3,4,5]
+        let expected =
+        """
+        ----top----
+        5
+        4
+        3
+        2
+        1
+        -----------
+        """
+        XCTAssertEqual(expected, sut.debugDescription)
+    }
+    
     func test_pop_returnsNilWithEmptyStack() {
         
         var sut = Stack<Int>()
         
+        print(sut.debugDescription)
+        
         XCTAssertNil(sut.pop())
+    }
+    
+    func test_pop_returnsElementWithFilledStack() {
+        
+        var sut: Stack<Int> = [1,2,3,4,5]
+        
+        print(sut.debugDescription)
+        
+        XCTAssertEqual(sut.pop(), 5)
+    }
+    
+    func test_push_addsElementAtEndOfEmptyStack() {
+        
+        var sut = Stack<Int>()
+        
+        sut.push(6)
+        
+        print(sut.debugDescription)
+        
+        XCTAssertEqual(sut.pop(), 6)
+    }
+    
+    func test_pop_addsElementAtEndOfFilledStack() {
+        
+        var sut: Stack<Int> = [1,2,3,4,5]
+        
+        sut.push(6)
+        
+        print(sut.debugDescription)
+        
+        XCTAssertEqual(sut.pop(), 6)
+    }
+    
+    func test_peek_returnsNilForEmptyStack() {
+        
+        let sut = Stack<Int>()
+        
+        print(sut.debugDescription)
+        
+        XCTAssertNil(sut.peek())
+    }
+    
+    func test_peek_returnsElementAtEndOfFilledStack() {
+        
+        let sut: Stack<Int> = [1,2,3,4,5]
+        
+        print(sut.debugDescription)
+        
+        XCTAssertEqual(sut.peek(), 5)
+    }
+    
+    func test_isEmpty_returnsTrueForEmptyStack() {
+        let sut = Stack<Int>()
+        
+        print(sut.debugDescription)
+        
+        XCTAssertTrue(sut.isEmpty())
+    }
+    
+    func test_isEmpty_returnsFalseForFilledStack() {
+        let sut: Stack<Int> = [1,2,3,4,5]
+        
+        print(sut.debugDescription)
+        
+        XCTAssertFalse(sut.isEmpty())
+    }
+    
+    func test_iterable_iteratesThroughEmptyStack() {
+        let sut = Stack<Int>()
+        
+        let expected = [Int]()
+        var result = [Int]()
+        
+        for elem in sut {
+            result.append(elem)
+        }
+        
+        XCTAssertEqual(result, expected)
+    }
+    
+    func test_iterable_iteratesThroughFilledStack() {
+        let sut: Stack<Int> = [1,2,3,4,5]
+        
+        let expected = [5,4,3,2,1]
+        var result = [Int]()
+        
+        for elem in sut {
+            result.append(elem)
+        }
+        
+        XCTAssertEqual(result, expected)
     }
 
 }
