72ac80cbbf6869abe38c6befa00810fe941a64c8
Jess Phillips <tripphillips5@gmail.com>
2022-04-28 17:59:55 -0500

    doc(LinkedListProtocols): update documentation and generic type name

--- LinkedList/LinkedList.swift
+++ LinkedList/LinkedList.swift
@@ -8,10 +8,10 @@
 import Foundation
 
 // MARK: - HeadTailLinkedList
-public struct LinkedList<Value>: HeadTailLinkedList {
+public struct LinkedList<T>: HeadTailLinkedList {
     
-    public var head: LinkedListNode<Value>?
-    public var tail: LinkedListNode<Value>?
+    public var head: LinkedListNode<T>?
+    public var tail: LinkedListNode<T>?
     
     public init() {}
 }
@@ -20,99 +20,45 @@ public struct LinkedList<Value>: HeadTailLinkedList {
 extension LinkedList: HeadTailLinkedListInspectable {
     
     public var isEmpty: Bool {
-        return head == nil
+    
     }
     
-    public func node(at index: Int) -> LinkedListNode<Value>? {
-        var currentNode = head
-        var currentIndex = 0
+    public func node(at index: Int) -> LinkedListNode<T>? {
         
-        while currentNode != nil && currentIndex < index {
-            currentNode = currentNode!.next
-            currentIndex += 1
-        }
-        
-        return currentNode
     }
 }
 
 // MARK: - HeadTailLinkedListInsertable
 extension LinkedList: HeadTailLinkedListInsertable {
     
-    public mutating func push(_ value: Value) {
-        head = LinkedListNode(value: value, next: head)
-        if tail == nil {
-            tail = head
-        }
+    public mutating func push(_ T: T) {
+        
     }
     
-    public mutating func append(_ value: Value) {
-        guard !isEmpty else {
-            push(value)
-            return
-        }
-        tail!.next = LinkedListNode(value: value, next: nil)
-        tail = tail!.next
+    public mutating func append(_ T: T) {
+       
     }
     
     @discardableResult
-    public mutating func insert(_ value: Value,
-                                after node: LinkedListNode<Value>) -> LinkedListNode<Value> {
-        guard tail !== node else {
-            append(value)
-            return tail!
-        }
+    public mutating func insert(_ T: T,
+                                after node: LinkedListNode<T>) -> LinkedListNode<T> {
         
-        node.next = LinkedListNode(value: value, next: node.next)
-        return node.next!
     }
 }
 
 // MARK: - HeadTailLinkedListRemovable
 extension LinkedList: HeadTailLinkedListRemovable {
-    public mutating func pop() -> Value? {
-        defer {
-            head = head?.next
-            if isEmpty {
-                tail = nil
-            }
-        }
+    public mutating func pop() -> T? {
         
-        return head?.value
     }
     
     @discardableResult
-    public mutating func removeLast() -> Value? {
-        guard let head = head else {
-            return nil
-        }
-        
-        guard head.next != nil else {
-            return pop()
-        }
-        
-        var previous = head
-        var current = head
-        
-        while let next = current.next {
-            previous = current
-            current = next
-        }
-        
-        previous.next = nil
-        tail = previous
-        return current.value
+    public mutating func removeLast() -> T? {
+       
     }
     
-    public mutating func remove(after node: LinkedListNode<Value>) -> Value? {
-        defer {
-            if node.next === tail {
-                tail = node
-            }
-            
-            node.next = node.next?.next
-        }
-        return node.next?.value
+    public mutating func remove(after node: LinkedListNode<T>) -> T? {
+        
     }
 }
 
--- LinkedList/LinkedListProtocols.swift
+++ LinkedList/LinkedListProtocols.swift
@@ -8,50 +8,61 @@
 import Foundation
 
 public protocol HeadTailLinkedList {
-    associatedtype Value
+    associatedtype T
     
     /// The first node in the linked list.
-    var head: LinkedListNode<Value>? { get set }
+    var head: LinkedListNode<T>? { get set }
     
     /// The last node in the linked list.
-    var tail: LinkedListNode<Value>? { get set }
-
+    var tail: LinkedListNode<T>? { get set }
 }
 
 public protocol HeadTailLinkedListInspectable {
-    associatedtype Value
+    associatedtype T
     
     /// Returns true if the linked list is empty.
     var isEmpty: Bool { get }
     
-    /// Returns the node at a given index.
-    func node(at index: Int) -> LinkedListNode<Value>?
+    /// Finds the node at an index and returns it.
+    /// - Parameters:
+    ///     - index: The index of the node beginning from 0.
+    /// - Returns: the node at a given index.
+    func node(at index: Int) -> LinkedListNode<T>?
 }
 
 public protocol HeadTailLinkedListInsertable {
-    associatedtype Value
+    associatedtype T
     
     /// adds a value at the front of the list.
-    mutating func push(_ value: Value)
+    mutating func push(_ value: T)
     
     /// adds a value at the end of the list.
-    mutating func append(_ value: Value)
+    mutating func append(_ value: T)
     
-    /// adds a value after a particular list node. Returns the
+    /// adds a value after a particular list node.
+    /// - Parameters:
+    ///     - value: The value to insert as a node.
+    ///     - after node: the node to insert the new node after.
+    /// - Returns: The node added
     @discardableResult
-    mutating func insert(_ value: Value, after node: LinkedListNode<Value>) -> LinkedListNode<Value>
+    mutating func insert(_ value: T, after node: LinkedListNode<T>) -> LinkedListNode<T>
 }
 
 public protocol HeadTailLinkedListRemovable {
-    associatedtype Value
+    associatedtype T
     
     /// Removes the value at the front of the list.
-    mutating func pop() -> Value?
+    /// - Returns: The value that was removed.
+    mutating func pop() -> T?
     
     /// Removes the value at the end of the list.
+    /// - Returns: The value that was removed.
     @discardableResult
-    mutating func removeLast() -> Value?
+    mutating func removeLast() -> T?
     
     /// Removes a value anywhere in the list.
-    mutating func remove(after node: LinkedListNode<Value>) -> Value?
+    /// - Parameters:
+    ///     - after node: the node that should have its next node removed.
+    /// - Returns: The value that was removed.
+    mutating func remove(after node: LinkedListNode<T>) -> T?
 }
