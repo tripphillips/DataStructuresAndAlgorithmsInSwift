5117cc5351e2121eb81f1b0b60fcaa9ed1dfe52c
Jess Phillips <tripphillips5@gmail.com>
2022-04-27 22:28:33 -0500

    task(LinkedList): create LinkedList and LinkedListTests targets

---
+++ LinkedList/LinkedList.h
@@ -0,0 +1,18 @@
+//
+//  LinkedList.h
+//  LinkedList
+//
+//  Created by Trip Phillips on 4/27/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LinkedList.
+FOUNDATION_EXPORT double LinkedListVersionNumber;
+
+//! Project version string for LinkedList.
+FOUNDATION_EXPORT const unsigned char LinkedListVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LinkedList/PublicHeader.h>
+
+
