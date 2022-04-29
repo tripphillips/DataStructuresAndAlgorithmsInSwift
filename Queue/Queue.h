0d414f450b4b1c7f2e07eb9e2151cd0e0cb7e31a
Jess Phillips <tripphillips5@gmail.com>
2022-04-29 12:30:40 -0500

    task(Queue): add Queue framework and tests targets

---
+++ Queue/Queue.h
@@ -0,0 +1,18 @@
+//
+//  Queue.h
+//  Queue
+//
+//  Created by Trip Phillips on 4/29/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Queue.
+FOUNDATION_EXPORT double QueueVersionNumber;
+
+//! Project version string for Queue.
+FOUNDATION_EXPORT const unsigned char QueueVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Queue/PublicHeader.h>
+
+
