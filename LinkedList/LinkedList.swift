0f7c353d5bd7436b32a2496edfe463dcbabba769
Jess Phillips <tripphillips5@gmail.com>
2022-04-28 22:26:47 -0500

    task(LinkedList): conform to Collection protocol

--- LinkedList/LinkedList.swift
+++ LinkedList/LinkedList.swift
@@ -124,3 +124,47 @@ extension LinkedList: CustomDebugStringConvertible {
     return String(describing: head)
   }
 }
+
+extension LinkedList: Collection {
+
+    public struct Index: Comparable {
+
+        public var node: LinkedListNode<T>?
+
+        static public func ==(lhs: Index, rhs: Index) -> Bool {
+            switch (lhs.node, rhs.node) {
+            case let (left?, right?):
+                return left.next === right.next
+            case (nil, nil):
+                return true
+            default:
+                return false
+            }
+        }
+
+        static public func <(lhs: Index, rhs: Index) -> Bool {
+            guard lhs != rhs else {
+                return false
+            }
+
+            let nodes = sequence(first: lhs.node) { $0?.next }
+            return nodes.contains { $0 === rhs.node }
+        }
+    }
+
+    public var startIndex: Index {
+        Index(node: head)
+    }
+
+    public var endIndex: Index {
+        Index(node: tail)
+    }
+    
+    public func index(after i: Index) -> Index {
+        Index(node: i.node?.next)
+    }
+    
+    public subscript(position: Index) -> T {
+        position.node!.value
+    }
+}
