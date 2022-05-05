06a2abe3766ed25e029e0b89aaa6eeaeb1ce2a8e
Jess Phillips <tripphillips5@gmail.com>
2022-05-05 17:40:18 -0500

    task(Binary Tree): add Binary Tree and Binary Tree tests

---
+++ BinaryTree/BinaryTree.h
@@ -0,0 +1,18 @@
+//
+//  BinaryTree.h
+//  BinaryTree
+//
+//  Created by Trip Phillips on 5/5/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BinaryTree.
+FOUNDATION_EXPORT double BinaryTreeVersionNumber;
+
+//! Project version string for BinaryTree.
+FOUNDATION_EXPORT const unsigned char BinaryTreeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BinaryTree/PublicHeader.h>
+
+
