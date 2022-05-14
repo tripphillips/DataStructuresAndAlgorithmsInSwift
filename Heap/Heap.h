c176c289f67e035e7d36ba3924cc95b24f7038e6
Jess Phillips <tripphillips5@gmail.com>
2022-05-14 18:38:17 -0500

    task(Heap): add Heap and Heap Tests

---
+++ Heap/Heap.h
@@ -0,0 +1,18 @@
+//
+//  Heap.h
+//  Heap
+//
+//  Created by Trip Phillips on 5/14/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Heap.
+FOUNDATION_EXPORT double HeapVersionNumber;
+
+//! Project version string for Heap.
+FOUNDATION_EXPORT const unsigned char HeapVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Heap/PublicHeader.h>
+
+
---
+++ HeapTests/HeapTests.swift
@@ -0,0 +1,58 @@
+//
+//  HeapTests.swift
+//  HeapTests
+//
+//  Created by Trip Phillips on 5/14/22.
+//
+
+import XCTest
+@testable import Heap
+
+class HeapTests: XCTestCase {
+
+    func test_insert_MaxHeap() {
+        var sut = makeSUT(sort: >)
+        sut.insert(2)
+        XCTAssertNotNil(sut.index(of: 2, startingAt: 0))
+    }
+    
+    func test_remove_MaxHeap() {
+        var sut = makeSUT(sort: >)
+        XCTAssertEqual(sut.remote(), 12)
+    }
+    
+    func test_removeAt_MaxHeap() {
+        var sut = makeSUT(sort: >)
+        XCTAssertEqual(sut.remote(at: 0), 12)
+    }
+    
+    func test_peek_MaxHeep() {
+        let sut = makeSUT(sort: >)
+        XCTAssertEqual(sut.peek(), 12)
+    }
+    
+    func test_insert_MinHeap() {
+        var sut = makeSUT(sort: <)
+        sut.insert(2)
+        XCTAssertNotNil(sut.index(of: 2, startingAt: 0))
+    }
+    
+    func test_remove_MinHeap() {
+        var sut = makeSUT(sort: <)
+        XCTAssertEqual(sut.remote(), 1)
+    }
+    
+    func test_removeAt_MinHeap() {
+        var sut = makeSUT(sort: <)
+        XCTAssertEqual(sut.remote(at: 0), 1)
+    }
+    
+    func test_peek_MinHeep() {
+        let sut = makeSUT(sort: <)
+        XCTAssertEqual(sut.peek(), 1)
+    }
+    
+    private func makeSUT(sort: @escaping (Int, Int) -> Bool) -> Heap<Int> {
+        return Heap<Int>(sort: sort, elements: [1,12,3,4,1,6,8,7])
+    }
+}
