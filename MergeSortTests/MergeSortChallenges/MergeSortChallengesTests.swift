87774d650a4e01f893692ace487aa7ab97e59bd3
Jess Phillips <tripphillips5@gmail.com>
2022-05-25 11:59:29 -0500

    task(MergeSort): add test for merging two sequences

--- MergeSortTests/MergeSortChallenges/MergeSortChallengesTests.swift
+++ MergeSortTests/MergeSortChallenges/MergeSortChallengesTests.swift
@@ -9,6 +9,13 @@ import XCTest
 
 class MergeSortChallengesTests: XCTestCase {
 
-   
+    func test_mergeTwoSequences_Challenge1() {
+        
+        let list1 = [1,4,6,9,10]
+        let list2 = [2,3,5,7,8]
+        
+        let result = Array(MergeSortChallenges.merge(first: list1, second: list2))
+        XCTAssertEqual(result, [1,2,3,4,5,6,7,8,9,10])
+    }
 
 }
